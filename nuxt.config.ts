4b65ddf96ac19a6eef0e1b6a4a70a5a2b39ab9d1
xingxingmofashu <xingxingmofashu@outlook.com>
2024-12-10 16:52:22 +0800

    chore: init repo

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true,
+  },
+  future: {
+    compatibilityVersion: 4
+  },
+  compatibilityDate: '2024-12-10'
+});
